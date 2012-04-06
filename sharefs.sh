a382874667577a540707c1e56b7b24812b8c577f
Markus Pielmeier <markus.pielmeier@googlemail.com>
2012-04-06 16:46:06 +0200

    implemented some directory creation
    implemented remote directory testing

--- sharefs.sh
+++ sharefs.sh
@@ -1,13 +1,56 @@
 fail()
 {
     echo "$1" >&2
+    echo "Aborting" >&2
     exit 1
+}
+
+ensureNotExisting()
+{
+    if [ -e "$1" ]
+    then
+	fail "$1 already existing"
+    fi
 }
 
 case "$1" in
     create)
 	# create a shared directory
 	# $ sharefs create targetDir [user@]host[:dir]
+
+	targetDir=$2
+	remoteDst=$3
+
+	# TODO validate arguments
+
+	targetName=`basename "$targetDir"`
+	shadowDir=$targetDir/../.$targetName.sharefs
+	configFile=$shadowDir/config
+	dataDir=$shadowDir/data
+	remoteAccount=`echo $remoteDst | sed 's/\([^:]*\):\(.*\)/\1/'`
+	remoteDir=`echo $remoteDst | sed 's/\([^:]*\):\(.*\)/\2/'`
+
+	if [ -z "$remoteAccount" -o -z "$remoteDir" ]
+	then
+	    fail "Invalid remote destination format: $remoteDst"
+	fi
+
+	ensureNotExisting "$targetDir"
+	ensureNotExisting "$shadowDir"
+
+	ssh "$remoteAccount" "test -e $remoteDir" && fail "Remote directory already exists: $remoteDst"
+
+	# TODO validate remote dst not yet exists
+
+	mkdir "$targetDir"
+	mkdir "$shadowDir"
+	mkdir "$dataDir"
+
+	echo "remoteDst=$remoteDst" > $configFile
+
+	# TODO create remote dst
+
+	echo "shadow: $shadowDir"
 
 	# TODO
 	;;
